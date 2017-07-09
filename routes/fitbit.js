3ced88df3661cdc3df1ea28cb08039f68d2cfbb0
Andromeda <g.simon.levett@gmail.com>
2017-07-09 17:37:39 +1000

    api to fitbit sorted

--- routes/fitbit.js
+++ routes/fitbit.js
@@ -12,10 +12,46 @@ var shr = new shared();
 
 shr.router.get('/trigger/:userid', function(req,res,next){
    res.json({'status':'trigger received, starting API calls, will populate weigh-ins'});
-   
-   
-   
-   
+    shr.mngC.connect(shr.url, function (err, db) {
+        var coll = db.collection('fitbit');
+        coll.find({'_userid': req.params._userid}).toArray(function (results) {
+            if (err) {
+                console.log(err);
+                res1.statusCode(500).end();
+            } else {
+                res1.json(results);
+
+            }
+            db.close();
+        });
+    });
+
+
+    var post_options = {
+        host: 'api.fitbit.com',
+        port: '443',
+        path: '/1/user/-/body/log/weight/date/2017-07-01/2017-07-09.json',
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Authorization': 'Bearer ' + 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI1VjQyTUYiLCJhdWQiOiIyMjhHRlQiLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJyd2VpIHJociByYWN0IHJwcm8iLCJleHAiOjE0OTk2MTMwNDAsImlhdCI6MTQ5OTU4NDI0MH0.vvymGzVj8YR-iiMQ8L5hei-sar1_NfK-1fblhrddNVo'
+        }
+    };
+
+
+    http1.get(post_options, function (res1) {
+
+        var body = '';
+        res1.on('data', function (chunk) {
+            body += chunk;
+        });
+        res1.on('end', function () {
+            console.log(body);
+        });
+        res1.on('error', function (err) {
+            console.log(err);
+        });
+    });
     
     
 });
