b532f9d6e50fb1df8c978841a681fb9b9eee3f85
Simon Levett <g.simon.levett@gmail.com>
2017-07-10 09:37:48 +0000

    fitbit token refresh

--- routes/fitbit.js
+++ routes/fitbit.js
@@ -17,9 +17,76 @@ const getAuthToken = function(_userid){
         var coll = db.collection('fitbit');
         console.log(req.params.userid);
         coll.find({'_userid' : _userid}).toArray(function (err,results) {
-            if(results[0].expireTime.valueOf() > Date.now()){
-                
+            if (err){
+                console.log(err);
+                return err;
             }
+            if(results){
+            if(results[0].expireTime.valueOf() > Date.now()){
+                return results[0];
+            } else {
+                // Build the post string from an object
+    var post_data = querystring.stringify({
+        'clientId': '228GFT',
+        'grant_type': "refresh_token",
+        'refresh_token': results[0].refresh_token
+        'redirect_uri': 'http://fitness.techtoedip.com:81/fitbit_success'
+
+    });
+
+  // An object of options to indicate where to post to
+  var post_options = {
+      host: 'api.fitbit.com',
+      port: '443',
+      path: '/oauth2/token',
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+          'Authorization': 'Basic MjI4R0ZUOmRhNmI2MGJiMWU0M2U4NTI3NTdjOTJhYmZiOWUxZTg3'
+      }
+  };
+
+  // Set up the request
+  var post_req = http1.request(post_options, function(res) {
+      res.setEncoding('utf8');
+      res.on('data', function (chunk) {
+          shr.mngC.connect(shr.url, function (err, db) {
+              var chunk1 = JSON.parse(chunk);
+              chunk1._userid = _userid;
+              chunk1.expireTime = new Date(Date.now() + (chunk1.expires_in * 100);
+              var coll = db.collection('fitbit');
+              coll.findOneAndReplace({_userid:chunk1._userid},chunk1,{upsert:true}, function (err, results) {
+                  if (err) {
+                      console.log(err);
+                      res1.statusCode(500).end();
+                  } else {
+                      res1.json(results);
+
+                  }
+                  db.close();
+              });
+          });
+      });
+  });
+
+  // post the data
+  post_req.write(post_data);
+  post_req.end();
+
+
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+            }}
+            
+            
             
     
     
@@ -132,9 +199,7 @@ shr.router.post('/', function (req, res1, next) {
           shr.mngC.connect(shr.url, function (err, db) {
               var chunk1 = JSON.parse(chunk);
               chunk1._userid = req.body._userid;
-              console.log(Date.now(),chunk1.expires_in);
-              chunk1.expireTime = new Date(Date.now() + chunk1.expires_in);
-              console.log(chunk1.expireTime.valueOf());
+              chunk1.expireTime = new Date(Date.now() + (chunk1.expires_in * 100);
               var coll = db.collection('fitbit');
               coll.findOneAndReplace({_userid:chunk1._userid},chunk1,{upsert:true}, function (err, results) {
                   if (err) {
