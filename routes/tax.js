a539a2b2fb20e4f3702b97002549f66627fa6a59
Simon Che <g.simon.levett@gmail.com>
2017-08-14 11:54:21 +0000

    groupby

--- routes/tax.js
+++ routes/tax.js
@@ -6,8 +6,30 @@
  public name:string,
  public cardio:boolean*/
 
+var mongoObject = require('mongodb').ObjectID;
 var shared = require('./shared');
 var shr = new shared();
+shr.router.get('/groupBy/day', function (req, res, next) {
+    shr.mngC.connect(shr.url, function (err, db) {
+        var collection = db.collection('taxTrans');
+        collection.aggregate([{'$group':{
+            '_id':{'Date':'$Date',Day:'$Day','Year-Month':'$Year-Month'},id:{$push:'$_id'},Details:{$push:'$Details'},Cost:{$push:'$Cost'},Account:{$push:'$Account'},Category:{$push:'$Category'}}
+        },{$sort:{'_id.Date':1}}], function(err,results){
+            if (err) {
+                        console.log(err);
+                        res.statusCode(500).end();
+                    } else {
+                        res.json(results);
+                    }
+            
+            
+            
+        });
+
+    });
+
+
+});
 
 shr.router.get('/', function (req, res, next) {
     shr.mngC.connect(shr.url, function (err, db) {
@@ -26,8 +48,9 @@ shr.router.get('/', function (req, res, next) {
 shr.router.post('/', function (req, res, next) {
     shr.mngC.connect(shr.url, function (err, db) {
         var insertDoc = req.body;
+        insertDoc._id =  new mongoObject.ObjectId(insertDoc._id);
 
-        var coll = db.collection('activity');
+        var coll = db.collection('taxTrans');
         coll.insertOne(insertDoc, function (err, results) {
             if (err) {
                 console.log(err);
