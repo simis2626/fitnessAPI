9c99a982db2b7032e2ca2022bab1b3859d950a1e
Simon Che <g.simon.levett@gmail.com>
2017-10-23 11:19:50 +0000

    TRying to make date filter work

--- routes/weigh-in.js
+++ routes/weigh-in.js
@@ -62,7 +62,7 @@ shr.router.get('/:userid', function (req, res, next) {
                     });
                     results = docs.concat(docs2);
                     var filterResults = results.filter(function (obj) {
-                        return obj.date.valueOf() >= new Date("2017-10-23").valueOf();
+                        return obj.date.valueOf() >= new Date("2017-10-22").valueOf();
                     });
 
                     var compfunc = function (dt1, dt2) {
